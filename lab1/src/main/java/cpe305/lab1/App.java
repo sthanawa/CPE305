743bc56505fc9f9c750e8b546334ef93362b429c
Salonee Thanawala <salonee@pcp046986pcs.wireless.calpoly.edu>
2017-04-03 15:08:20 -0700

    Setup maven

---
+++ lab1/src/main/java/cpe305/lab1/App.java
@@ -0,0 +1,13 @@
+package cpe305.lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
